4ee24f111b9899a8c0303455a78abe40205b2148
Idelis Gé Ramírez <idelis.ge@gmail.com>
2019-06-11 12:00:32 -0400

    [IMP] Redefine the button_confirm method.

--- cPurchase/models/models.py
+++ cPurchase/models/models.py
@@ -128,6 +128,23 @@ class PurchaseOrder(models.Model):
                              string='Status', readonly=True,
                              index=True, copy=False,
                              default='draft', track_visibility='onchange')
+    @api.multi
+    def button_confirm(self):
+        for order in self:
+            if order.state not in ['draft', 'sent']:
+                continue
+            order._add_supplier_to_product()
+            # Deal with double validation process
+            if order.company_id.po_double_validation == 'one_step'\
+                    or (order.company_id.po_double_validation == 'two_step'\
+                        and order.amount_total < self.env.user.company_id.currency_id._convert(
+                            order.company_id.po_double_validation_amount, order.currency_id, order.company_id, order.date_order or fields.Date.today()))\
+                    or order.user_has_groups('purchase.group_purchase_manager'):
+                order.button_approve()
+            else:
+                order.write({'state': 'purchase'})
+        return True
+
     @api.model
     def create(self, vals):
         ''' Is necessary redefine for find the department who belong the
